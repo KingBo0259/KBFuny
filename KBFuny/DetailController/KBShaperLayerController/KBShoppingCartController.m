43a2205d7b16a687f06ab27808b25f82e72e55e1
jinlb <jlb@kuaihuoyun.com>
2016-07-22 15:44:29 +0800

    类似购物车 选择效果

---
+++ KBFuny/DetailController/KBShaperLayerController/KBShoppingCartController.m
@@ -0,0 +1,13 @@
+//
+//  KBShoppingCartController.m
+//  KBFuny
+//
+//  Created by jinlb on 16/7/22.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "KBShoppingCartController.h"
+
+@implementation KBShoppingCartController
+
+@end
