16edcfd17f9c4d7071cd6c1e2b1836d8bf7aa543
jinlb <jlb@kuaihuoyun.com>
2016-08-30 16:03:08 +0800

    模仿Uber

---
+++ KBFuny/DetailController/KBShaperLayerController/Uber/KBUberImitateViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBUberImitateViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 16/8/17.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBUberImitateViewController : UIViewController
+
+@end
---
+++ KBFuny/DetailController/KBShaperLayerController/Uber/KBUberImitateViewController.m
@@ -0,0 +1,37 @@
+//
+//  KBUberImitateViewController.m
+//  KBFuny
+//
+//  Created by jinlb on 16/8/17.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "KBUberImitateViewController.h"
+
+@interface KBUberImitateViewController ()
+
+@end
+
+@implementation KBUberImitateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
