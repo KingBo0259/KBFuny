35e59185da841fad8ea9f2042da65db4998665b1
jinlb <jlb@kuaihuoyun.com>
2016-08-30 16:03:18 +0800

    模仿Uber

--- KBFuny/DetailController/KBShaperLayerController/Uber/KBUberImitateViewController.h
+++ KBFuny/DetailController/KBShaperLayerController/Uber/KBUberImitateViewController.h
@@ -1,7 +1,7 @@
 //
 //  KBUberImitateViewController.h
 //  KBFuny
-//
+//   模仿 Uber 效果
 //  Created by jinlb on 16/8/17.
 //  Copyright © 2016年 jinlb. All rights reserved.
 //
--- KBFuny/DetailController/KBShaperLayerController/Uber/KBUberImitateViewController.m
+++ KBFuny/DetailController/KBShaperLayerController/Uber/KBUberImitateViewController.m
@@ -17,11 +17,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.title=@"Uber demo";
+    
+    self.view.backgroundColor=[UIColor yellowColor];
+    
+    CALayer *layer=[self createCirleLayer];
+    [self.view.layer addSublayer:layer];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(CALayer*)createRect{
+
+    
+    UIBezierPath *bezierLayer= [UIBezierPath bezierPathWithRect:CGRectMake(100, 100, 10, 10)];
+    CAShapeLayer *shaperLayer=[CAShapeLayer layer];
+    shaperLayer.path=bezierLayer.CGPath;
+    shaperLayer.lineWidth=1.0;
+    shaperLayer.strokeColor=[UIColor yellowColor].CGColor;
+    shaperLayer.fillColor=[UIColor yellowColor].CGColor;
+    
+    
+
+    
+    
+    return shaperLayer;
+
+}
+
+-(CAShapeLayer*)createCirleLayer{
+
+    
+    CGFloat width=50.0f;
+    UIBezierPath *bezierPath=[UIBezierPath bezierPathWithOvalInRect:CGRectMake(100, 100, width, width)];
+    
+    CAShapeLayer *shaperLayer=[CAShapeLayer layer];
+    shaperLayer.path=bezierPath.CGPath;
+    shaperLayer.lineWidth=width;
+    shaperLayer.strokeColor=[UIColor blueColor].CGColor;
+    shaperLayer.fillColor=[UIColor clearColor].CGColor;
+    
+    //添加动画
+    CABasicAnimation *pathAnima=[CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    pathAnima.duration=2.0f;
+    pathAnima.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    pathAnima.fromValue=@0.0f;
+    pathAnima.toValue=@1.0f;
+    pathAnima.fillMode=kCAFillModeForwards;
+    pathAnima.removedOnCompletion=YES;
+
+    [shaperLayer addAnimation:pathAnima forKey:@"strokeEndAnimation"];
+    return shaperLayer;
 }
 
 /*
