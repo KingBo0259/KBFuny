96af8466c253f33839f683252e04b39d81d7594a
jinlb <jlb@kuaihuoyun.com>
2016-08-31 09:12:47 +0800

    图片折叠

---
+++ KBFuny/DetailController/KBShaperLayerController/ImageFloder/KBImageFloderTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBImageFloderTableViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 16/8/30.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBImageFloderTableViewController : UITableViewController
+
+@end
