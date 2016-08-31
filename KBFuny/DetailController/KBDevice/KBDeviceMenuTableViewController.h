347ca7eae4fc9e22b94cad51deaaa969425ec323
jinlb <jlb@kuaihuoyun.com>
2016-08-31 15:27:43 +0800

    add  扫描功能

---
+++ KBFuny/DetailController/KBDevice/KBDeviceMenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBDeviceMenuTableViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 16/8/31.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBDeviceMenuTableViewController : UITableViewController
+
+@end
---
+++ KBFuny/DetailController/KBDevice/scan/KBScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBScanViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 16/8/31.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBScanViewController : UIViewController
+
+@end
