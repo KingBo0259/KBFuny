c6087952708a5052f38810904524a7232ef2b8bc
jinlb <jlb@kuaihuoyun.com>
2016-09-06 15:17:15 +0800

    指纹识别  代码添加

---
+++ KBFuny/DetailController/KBDevice/FingerUnLock/KBFingerUnLockViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBFingerUnLockViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 16/9/6.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBFingerUnLockViewController : BaseViewController
+
+@end
---
+++ KBFuny/DetailController/KBDevice/FingerUnLock/KBFingerUnLockViewController.m
@@ -0,0 +1,13 @@
+//
+//  KBFingerUnLockViewController.m
+//  KBFuny
+//
+//  Created by jinlb on 16/9/6.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "KBFingerUnLockViewController.h"
+
+@implementation KBFingerUnLockViewController
+
+@end
