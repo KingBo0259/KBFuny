15b7fd935bbd8f955d601c5598f5dda4f1d6baf6
jinlb <jlb@kuaihuoyun.com>
2016-09-08 16:21:29 +0800

    add  Facebook pop

---
+++ KBFuny/DetailController/KBPop/KBPOPBasicAnimation/KBPOPBasicAnimationViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBPOPBasicAnimationViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 16/9/8.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBPOPBasicAnimationViewController : UIViewController
+
+@end
---
+++ KBFuny/DetailController/KBPop/KBPOPBasicAnimation/KBPOPBasicAnimationViewController.m
@@ -0,0 +1,37 @@
+//
+//  KBPOPBasicAnimationViewController.m
+//  KBFuny
+//
+//  Created by jinlb on 16/9/8.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "KBPOPBasicAnimationViewController.h"
+
+@interface KBPOPBasicAnimationViewController ()
+
+@end
+
+@implementation KBPOPBasicAnimationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
