8b6dfdeed615598bfb253f7b3524156953cbf46c
jinlb <jlb@kuaihuoyun.com>
2016-09-09 14:52:06 +0800

    category  利用 associate 变向实现成员变量的功能

---
+++ KBFuny/DetailController/KBObjectiveC/AssAssociatedobject/UIButton+KB.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+KB.h
+//  KBFuny
+//
+//  Created by jinlb on 16/9/9.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (KB)
+
+@end
---
+++ KBFuny/DetailController/KBObjectiveC/AssAssociatedobject/UIButton+KB.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+KB.m
+//  KBFuny
+//
+//  Created by jinlb on 16/9/9.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "UIButton+KB.h"
+
+@implementation UIButton (KB)
+
+@end
