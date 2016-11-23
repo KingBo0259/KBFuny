6cd0b1a7318aac9e97054ab9fd924b5e4bc53058
jinlb <jlb@kuaihuoyun.com>
2016-11-23 17:45:06 +0800

    add  textField

---
+++ KBFuny/Custom/KBTextField/KBTextField.h
@@ -0,0 +1,13 @@
+//
+//  KBTextField.h
+//  KBFuny
+//
+//  Created by jinlb on 2016/11/19.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBTextField : UITextField
+
+@end
---
+++ KBFuny/Custom/KBTextField/KBTextField.m
@@ -0,0 +1,21 @@
+//
+//  KBTextField.m
+//  KBFuny
+//
+//  Created by jinlb on 2016/11/19.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "KBTextField.h"
+
+@implementation KBTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
