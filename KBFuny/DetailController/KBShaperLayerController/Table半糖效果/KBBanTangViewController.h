88cfa812a4d7a452e580ba41fe841dd025578587
jinlb <jlb@kuaihuoyun.com>
2017-01-06 17:56:58 +0800

    add

---
+++ KBFuny/DetailController/KBShaperLayerController/Table半糖效果/KBBanTangViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBBanTangViewController.h
+//  KBFuny
+//
+//  Created by jinlb on 2016/12/28.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBBanTangViewController : UIViewController
+
+@end
