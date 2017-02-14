8c82797bce28fc290342203f9b4129772df95cda
jinlb <jlb@kuaihuoyun.com>
2017-02-14 11:50:42 +0800

    跑马灯 文字效果

---
+++ KBFuny/Custom/KBFadeString/KBFadeString.h
@@ -0,0 +1,13 @@
+//
+//  KBFadeString.h
+//  KBFuny
+//
+//  Created by jinlb on 2017/2/14.
+//  Copyright © 2017年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBFadeString : UIView
+
+@end
---
+++ KBFuny/Custom/KBFadeString/KBFadeString.m
@@ -0,0 +1,21 @@
+//
+//  KBFadeString.m
+//  KBFuny
+//
+//  Created by jinlb on 2017/2/14.
+//  Copyright © 2017年 jinlb. All rights reserved.
+//
+
+#import "KBFadeString.h"
+
+@implementation KBFadeString
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
