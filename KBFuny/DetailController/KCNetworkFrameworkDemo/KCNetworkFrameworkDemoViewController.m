8451b2b3c00c4f357141302b922cd05ad03ba302
jinlingbo <jinlingbo@u51.com>
2018-02-28 20:35:33 +0800

    [ADD] 信号量实现，网络异步请求的同步等待

--- KBFuny/DetailController/KCNetworkFrameworkDemo/KCNetworkFrameworkDemoViewController.m
+++ KBFuny/DetailController/KCNetworkFrameworkDemo/KCNetworkFrameworkDemoViewController.m
@@ -20,14 +20,32 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor =[UIColor whiteColor];
-    
-    [self demo];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button addTarget:self action:@selector(demo) forControlEvents:UIControlEventTouchUpInside];
+    button.frame = CGRectMake(10, 64, 200, 64);;
+    [button setTintColor:[UIColor redColor]];
+    [button setTitle:@"信号量" forState:UIControlStateNormal];
+    [self.view addSubview:button];
 }
 
 
 -(void)demo{
-
-
+    // 设置一个异步线程组
+        // 设置一个网络请求
+        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.github.com"]];
+        // 创建一个信号量为0的信号(红灯)
+        dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+        NSURLSessionDownloadTask *task = [[NSURLSession sharedSession] downloadTaskWithRequest:request completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            NSLog(@"第一步操作");
+            // 使信号的信号量+1，这里的信号量本来为0，+1信号量为1(绿灯)
+            dispatch_semaphore_signal(sema);
+        }];
+        [task resume];
+        // 以下还要进行一些其他的耗时操作
+        NSLog(@"耗时操作继续进行");
+        // 开启信号等待，设置等待时间为永久，直到信号的信号量大于等于1（绿灯）
+        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+        NSLog(@"dispatch_semaphore_wait_end");
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
